bfd66547aafc60ba761cf991b3ad19439314d3d4
StefanoPruna <48112000+StefanoPruna@users.noreply.github.com>
2019-06-25 15:12:43 +1000

    First Commit

---
+++ RobotRabbit/Assets/Scripts/GameManager.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject robot;
+
+    public InputField txtX;
+    public InputField txtY;
+    public Dropdown dropdownDirection;
+
+    Rigidbody2D myRB;
+    SpriteRenderer myRend;
+    Animator Anim;
+
+    bool facingRight = true;
+
+    public Sprite UpSprite;
+    public Sprite DownSprite;
+    public Sprite RightSprite;
+    public Sprite LeftSprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myRend = GetComponent<SpriteRenderer>();
+        Anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Place()
+    {
+        int x = Convert.ToInt32(txtX.text);
+        int y = Convert.ToInt32(txtY.text);
+        string direction = dropdownDirection.options[dropdownDirection.value].text;
+
+        if (x >= 3 || x <= -3)        
+            return;
+        if (y >= 3 || y <= -3)
+            return;                              
+
+        robot.SetActive(true);
+
+        //Change the position
+        robot.transform.position = new Vector2(x, y);  
+        
+        //Change the direction
+        switch(direction.ToLower())
+        {
+            case "north":
+                robot.GetComponent<SpriteRenderer>().sprite = UpSprite;
+                break;
+            case "south":
+                robot.GetComponent<SpriteRenderer>().sprite = DownSprite;
+                break;
+            case "west":
+                robot.GetComponent<SpriteRenderer>().sprite = LeftSprite;
+                break;
+            case "east":
+                robot.GetComponent<SpriteRenderer>().sprite = RightSprite;
+                break;
+        }
+       
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        myRend.flipX = !myRend.flipX;
+    }
+
+}
---
+++ RobotRabbit/Assets/Scripts/Move.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    Rigidbody2D myRB;
+    SpriteRenderer myRend;
+    Animator Anim;
+    bool facingRight = true;
+    public float MoveSpeed;
+
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        myRend = GetComponent<SpriteRenderer>();
+        Anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        bool isMove = false;
+
+        float lh = Input.GetAxis("Horizontal");
+        float lv = Input.GetAxis("Vertical");
+
+        if (lh != 0 || lv != 0) isMove = true;
+
+        if (lh < 0 && !facingRight)
+            Flip();
+        else if (lh > 0 && facingRight)
+            Flip();
+
+        myRB.velocity = new Vector2(lh * MoveSpeed,lv * MoveSpeed);    
+        
+        
+        Anim.SetBool("Move", isMove);
+      
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        myRend.flipX = !myRend.flipX;
+    }
+}
